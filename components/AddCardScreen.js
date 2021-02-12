4ed7e298737e0e1846918e77b23fe5c52e8732f0
XtratoS <ultimate.nuker@yahoo.com>
2021-02-12 07:17:32 +0200

    Added missing Dependancy

--- components/AddCardScreen.js
+++ components/AddCardScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, Platform } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import { connect } from 'react-redux'
 import { handleAddCard } from '../actions'
